4bf82cfeda8c5fa0f48fbd4afe2984be4fd61f7a
Nguyễn Anh Tuấn <nguyenanhtuan2111nd@gmail.com>
2026-08-26 15:32:51 +0700

    Step 19: mask_attention_scores_with_neg_inf

--- model.py
+++ model.py
@@ -184,8 +184,14 @@ def scale_attention_scores(scores, d_k):
     scales = math.sqrt(d_k)
     return scores / scales
 
-# Step 19 - mask_attention_scores_with_neg_inf (not yet solved)
-# TODO: implement
+# Step 19 - mask_attention_scores_with_neg_inf
+import torch
+
+def mask_attention_scores_with_neg_inf(scores, mask):
+    """Set entries of scores where mask is False to -inf."""
+    # TODO: replace blocked positions of scores with negative infinity
+    scores = torch.where(mask == False, -torch.inf, scores)
+    return scores
 
 # Step 20 - softmax_attention_weights (not yet solved)
 # TODO: implement
