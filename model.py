400d387fca60d60771b4a3730d8cce21329c346d
Nguyễn Anh Tuấn <nguyenanhtuan2111nd@gmail.com>
2026-09-06 15:13:22 +0700

    Step 30: merge_heads_and_project_output

--- model.py
+++ model.py
@@ -283,8 +283,14 @@ def multi_head_scaled_dot_product_attention(q_h, k_h, v_h, mask=None):
     
     return scaled_dot_product_attention(q_h, k_h, v_h, mask)
 
-# Step 30 - merge_heads_and_project_output (not yet solved)
-# TODO: implement
+# Step 30 - merge_heads_and_project_output
+import torch
+
+def merge_heads_and_project_output(context, w_o, b_o):
+    # TODO: merge the head axis back into d_model and apply the output linear projection.
+    merge = merge_heads_back_to_model_dim(context)
+    output = apply_linear_projection(merge, w_o, b_o)
+    return output
 
 # Step 31 - assemble_multi_head_attention_forward (not yet solved)
 # TODO: implement
